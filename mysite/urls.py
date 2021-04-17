dd40324b782860ebd2a46bf25c84561d6be8e657
innotuyi <innotuyi>
2021-04-17 13:16:33 +0200

    Adding serve

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as authentication_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import server
+from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
